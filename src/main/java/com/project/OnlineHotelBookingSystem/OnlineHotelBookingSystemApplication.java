19dd98b8dd3c141dc942ca0fcf8864d0d92a86fe
ermias111 <ermimichael@gmail.com>
2021-06-23 23:07:03 -0500

    initial commit

---
+++ src/main/java/com/project/OnlineHotelBookingSystem/OnlineHotelBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.OnlineHotelBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineHotelBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineHotelBookingSystemApplication.class, args);
+	}
+
+}
