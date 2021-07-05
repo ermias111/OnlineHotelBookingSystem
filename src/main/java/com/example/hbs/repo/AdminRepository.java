305a4a2bcf5c0a349b7dae0ca5a1c8a1e5f9e5f9
ray100 <jraymonddoirin@gmail.com>
2021-07-04 22:35:51 -0500

    add default admin

--- src/main/java/com/example/hbs/repo/AdminRepository.java
+++ src/main/java/com/example/hbs/repo/AdminRepository.java
@@ -3,12 +3,58 @@ package com.example.hbs.repo;
 import com.example.hbs.domain.Admin;
 import com.example.hbs.domain.Hotel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Optional;
 
 public interface AdminRepository  extends CrudRepository<Admin, Integer> {
     Optional<Admin> findByUsername(String userName);
 
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    <S extends Admin> S save(S entity);
 
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    <S extends Admin> Iterable<S> saveAll(Iterable<S> entities);
 
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    Optional<Admin> findById(Integer integer);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    boolean existsById(Integer integer);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    Iterable<Admin> findAll();
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    Iterable<Admin> findAllById(Iterable<Integer> integers);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    long count();
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    void deleteById(Integer integer);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    void delete(Admin entity);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    void deleteAll(Iterable<? extends Admin> entities);
+
+    @Override
+    @PreAuthorize("hasAuthority('Admin')")
+    void deleteAll();
 }
